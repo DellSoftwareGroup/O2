0c94f0054621a695765b66e8fa85d4fbe4eaa677
Jorge A Leon <jorge@frontendify.com>
2016-01-27 19:02:41 -0800

    Major Folder structure changes (added Bootstrap) + pagination

--- SingleQueue/static/js/custom-ui.js
+++ SingleQueue/static/js/custom-ui.js
@@ -48,138 +48,258 @@ var templateLoader = (function($,host){
 */
 var singleQueueTables = (function($){
 
+ /* ------->  Private functions and methods  <------- */
 
-	// common functions for all tables
-	var commonStyles = (function() {
-		function summarySecStyles(numCol){
-			$('th').each(function () {
-				if ($(this).data("index") > numCol) {
-					$(this).addClass('summary-th');
-				}
-			})
-		};
-		function summaryBorder(colClass){
-			$(colClass).parent('td').addClass('summary-start');
-		};
+      // common functions for all tables - Table DOM manipulations
+      var commonStyles = (function() {
+          function summarySecStyles(numCol){
+            $('th').each(function () {
+              if ($(this).data("index") > numCol) {
+                $(this).addClass('summary-th');
+              }
+            })
+          };
+          function summaryBorder(colClass){
+            $(colClass).parent('td').addClass('summary-start');
+          };
 
-		// TODO: temp function should be removed by Eder (Styles to match mocks)
-		function tempStyles(){
+          // TODO: temp function should be removed by Eder (Styles to match mocks)
+          function tempStyles(){
 
-			// urgent, new and legacy labels
-			var taggedCol = $('.title-col'),
-					label = ['<span class="icon urgent">Urgent</span>',
-						'<span class="icon new">New</span>',
-						'<span class="icon legacy">Legacy</span>',
-						'<span class="icon legacy">Legacy</span>'];
+            // urgent, new and legacy labels
+            var taggedCol = $('.title-col'),
+                label = ['<span class="icon urgent">Urgent</span>',
+                  '<span class="icon new">New</span>',
+                  '<span class="icon legacy">Legacy</span>',
+                  '<span class="icon legacy">Legacy</span>'];
 
-			for (var i = 0; i < taggedCol.length; i++) {
-				$(taggedCol[i]).find('a').append(label[i]);
-			}
+            for (var i = 0; i < taggedCol.length; i++) {
+              $(taggedCol[i]).find('a').append(label[i]);
+            }
 
-			// late warning label
-			var lateCol = ['.endDate-col', '.requestEndDate-col'];
-			lateCol.forEach(function (element) {
-				var temp = $(element)[0];
-				$(temp).append('<span class="icon past-due"></span>');
-			});
-		};
+            // late warning label
+            var lateCol = ['.endDate-col', '.requestEndDate-col'];
+            lateCol.forEach(function (element) {
+              var temp = $(element)[0];
+              $(temp).append('<span class="icon past-due"></span>');
+            });
+          };
 
-		// pagination
-		function GridRequest_DataBound(){
-			var pager = $("#BottomPager").kendoPager({
-				dataSource: $("#GridRequest").data("kendoGrid").dataSource,
-				pageSize: 10,
-				autoBind: true,
-				selectTemplate: '<li class="active"><a class="data-pager-link" data-page="#=text#">#=text#</a></li>',
-				linkTemplate: '<li><a class="data-pager-link" href="\\#" data-#=ns#page="#=text#">#=text#</a></li>',
-				messages: {
-					display: '<li class="">{0} - {1} of {2}</li>'
-				}
-			}).data("kendoPager");
+          // pagination
+          function GridRequest_DataBound(){
 
-			$(".k-i-seek-w").parent().hide();
-			$(".k-i-seek-e").parent().hide();
+            // ---> Top Pagination Starts
+              var pager = $("#TopPager").kendoPager({
+                dataSource: $("#GridRequest").data("kendoGrid").dataSource,
+                pageSize: 50,
+                autoBind: true,
+                selectTemplate: '<li class="active"><a class="data-pager-link" data-page="#=text#">#=text#</a></li>',
+                linkTemplate: '<li><a class="data-pager-link" href="\\#" data-#=ns#page="#=text#">#=text#</a></li>',
+                messages: {
+                  display: '<li class="">{0} - {1} of {2}</li>'
+                }
+              });
 
-			$('#BottomPager>ul').prepend("<li></li>");
-			$('#BottomPager .k-i-arrow-w').parent().addClass("prev").detach().appendTo('#BottomPager>ul li:first').find("span").remove();
-			$('#BottomPager>ul').append("<li></li>");
-			$('#BottomPager .k-i-arrow-e').parent().addClass("next").detach().appendTo('#BottomPager>ul li:last').find("span").remove();
-			$('#BottomPager>ul').prepend("<li></li>");
-			$('#BottomPager .k-pager-info').detach().appendTo('#BottomPager>ul li:first');
+              $(".k-i-seek-w").parent().hide();
+              $(".k-i-seek-e").parent().hide();
 
-			$(".data-pager-link").click(function (e) {
-				//return CheckDataChanges();
-        console.log("pager-link");
-			});
-			$(".k-pager-nav").click(function (e) {
-				//return CheckDataChanges();
-        console.log("pager-nav");
-			});
-		}
+              $('#TopPager>ul').prepend("<li></li>");
+              $('#TopPager .k-i-arrow-w').parent().addClass("prev").detach().appendTo('#TopPager>ul li:first').find("span").remove();
+              $('#TopPager>ul').append("<li></li>");
+              $('#TopPager .k-i-arrow-e').parent().addClass("next").detach().appendTo('#TopPager>ul li:last').find("span").remove();
+              $('#TopPager>ul').prepend("<li></li>");
+              $('#TopPager .k-pager-info').detach().appendTo('#TopPager>ul li:first');
 
-		return {
-			summarySecStyles: summarySecStyles,
-			summaryBorder: summaryBorder,
-			tempStyles: tempStyles,
-			GridRequest_DataBound: GridRequest_DataBound
-		}
-	}());
 
-  // Add color tags to owner field
-  var ownerColorTag = function(){
-    var colorToName = [
-      { name: 'James Gomez', color: "#6ea204"},
-      { name: 'Michael Hughes', color: "#42aeaf"},
-      { name: 'Giovani Monsalve', color : "#ee6411"}
-    ]
-    // pick up owner name
-    $('.owner-col').each(function(){
-      var ownerName = $(this).find('.owner-name').text();
-      var $that = $(this);
+            // ---> Bottom Pagination Starts
+              var pager = $("#BottomPager").kendoPager({
+                dataSource: $("#GridRequest").data("kendoGrid").dataSource,
+                pageSize: 10,
+                autoBind: true,
+                selectTemplate: '<li class="active"><a class="data-pager-link" data-page="#=text#">#=text#</a></li>',
+                linkTemplate: '<li><a class="data-pager-link" href="\\#" data-#=ns#page="#=text#">#=text#</a></li>',
+                messages: {
+                  display: '<li class="">{0} - {1} of {2}</li>'
+                }
+              })
+              console.log('pager: ' + pager);
+              $(".k-i-seek-w").parent().hide();
+              $(".k-i-seek-e").parent().hide();
 
-      // compare and if match add styles
-      colorToName.forEach(function(owner){
-        if (owner.name == ownerName){
-          $that.find('span:first-child')
-            .css('background-color', owner.color)
-            .addClass('color-id');
+              $('#BottomPager>ul').prepend("<li></li>");
+              $('#BottomPager .k-i-arrow-w').parent().addClass("prev").detach().appendTo('#BottomPager>ul li:first').find("span").remove();
+              $('#BottomPager>ul').append("<li></li>");
+              $('#BottomPager .k-i-arrow-e').parent().addClass("next").detach().appendTo('#BottomPager>ul li:last').find("span").remove();
+              $('#BottomPager>ul').prepend("<li></li>");
+              $('#BottomPager .k-pager-info').detach().appendTo('#BottomPager>ul li:first');
+
+              $(".data-pager-link").click(function (e) {
+                //return CheckDataChanges();
+                console.log("pager-link");
+              });
+              $(".k-pager-nav").click(function (e) {
+                //return CheckDataChanges();
+                console.log("pager-nav");
+              });
+          }
+
+          return {
+            summarySecStyles: summarySecStyles,
+            summaryBorder: summaryBorder,
+            tempStyles: tempStyles,
+            GridRequest_DataBound: GridRequest_DataBound
+          }
+      }());
+
+      // Add color tags to owner field
+      var ownerColorTag = function(){
+      var colorToName = [
+        { name: 'James Gomez', color: "#6ea204"},
+        { name: 'Michael Hughes', color: "#42aeaf"},
+        { name: 'Giovani Monsalve', color : "#ee6411"}
+      ]
+      // pick up owner name
+      $('.owner-col').each(function(){
+        var ownerName = $(this).find('.owner-name').text();
+        var $that = $(this);
+
+        // compare and if match add styles
+        colorToName.forEach(function(owner){
+          if (owner.name == ownerName){
+            $that.find('span:first-child')
+              .css('background-color', owner.color)
+              .addClass('color-id');
+          }
+        });
+
+      })
+      };
+
+      //Kendo Window widget + Grid
+      var modalTable = (function(data){
+        // data will come from IS
+        // for now we are using this sample data
+        var dataObj = [
+          {
+            "id": "27139",
+            "title": "Active Administrator for DNS Management",
+            "type": "Tech Brief"
+          },
+          {
+            "id": "47554",
+            "title": "TZ 215 Product Page",
+            "type": "Blog Post Series"
+          },
+          {
+            "id": "MRCM-47411",
+            "title": "TX Series White Papers",
+            "type": "White Paper"
+          }];
+
+        function dataFilter(){
+          return dataObj;
         }
-      });
 
-    })
-  };
+        function makeTable(){
+          var dataSet = dataFilter();
 
-  var requestListings = {
-    styles: function(){
-      var summaryIndx = 11,  summaryStartCol =".group-col";
-      commonStyles.summarySecStyles(summaryIndx);
-      commonStyles.summaryBorder(summaryStartCol);
-      commonStyles.tempStyles();
-      //commonStyles.GridRequest_DataBound();
-    }
-  };
+          // Using Kendo Grid to create table Dynamically
+          $('#modalTable').kendoGrid({
+            columns: [{title: "ID", field: "id",
+              template: "<div class='id-col'>#= id #</div>"},
+              {title: "Title", field: "title",
+                template: "<div class='title-col'><a href=''>#= title #</a></div>"},
+              {title: "Type", field: "type",
+                template: "<div class='type-col'>#= type #</div>"}],
+            dataSource: {
+              data: dataSet,
+              pageSize: 10,
+              page: 1,
+              total: 0
+            },
+          });
 
-	var taskListings =  {
-		styles: function(){
-			var summaryIndx =9,  summaryStartCol =".sprint-col";
-			commonStyles.summarySecStyles(summaryIndx);
-			commonStyles.summaryBorder(summaryStartCol);
-			commonStyles.tempStyles();
-      commonStyles.GridRequest_DataBound();
-      ownerColorTag();
-		}
-	};
+        }
+
+        function activateKendoWindow() {
+
+          // Use of Kendo "Window" widget for Modal
+          var myWindow = $('#modalWrapper'),
+            undo = $("#undo");
+
+          undo.click(function() {
+            myWindow.data("kendoWindow").open();
+            undo.fadeOut();
+          });
+
+          function onClose() {
+            undo.fadeIn();
+          }
+
+          myWindow.kendoWindow({
+            width: "600px",
+            title: "test Table",
+            visible: false,
+            actions: [
+              "Close"
+            ],
+            close: onClose
+          }).data("kendoWindow").center().open();
+        }
+        /* End of Kendo "Window" widget code */
 
 
+       // Public method
+        function init(){
+          $('body').on('hover', '.active-col', function(){
+            activateKendoWindow();
+            makeTable();
+          })
+        }
+
+       // API
+        return {
+          init : init
+        }
+      }());
 
 
+  /* ------->  Public methods  <------- */
+
+      // Request Listing Table
+      var requestListings = {
+        styles: function(){
+          var summaryIndx = 11,  summaryStartCol =".group-col";
+          commonStyles.summarySecStyles(summaryIndx);
+          commonStyles.summaryBorder(summaryStartCol);
+          commonStyles.tempStyles();
+          commonStyles.GridRequest_DataBound();
+          modalTable.init();
+        }
+      };
+
+      // Task Listing Table
+      var taskListings =  {
+        styles: function(){
+          var summaryIndx =9,  summaryStartCol =".sprint-col";
+          commonStyles.summarySecStyles(summaryIndx);
+          commonStyles.summaryBorder(summaryStartCol);
+          commonStyles.tempStyles();
+          commonStyles.GridRequest_DataBound();
+          ownerColorTag();
+        }
+      };
+
+
+  /* ------->  API  <------- */
 	return {
 		requestListings: requestListings,
-		taskListings: taskListings,
-		commonStyles: commonStyles
+		taskListings: taskListings
 	}
 
 }(jQuery));
+
+
 
 
 
