9f35b896ea73d7a9080fe2607af3a95fcb9c8f59
Edward Chong <edward@ed-mart.com>
2016-06-30 10:41:02 -0700

    fixed single queue template wrapper.
    Change-Id: Ife451a11ce50d044c42fee1cb4ad05d776884dc8

--- SQ/singlequeue/pages/index.php
+++ SQ/singlequeue/pages/index.php
@@ -4,12 +4,12 @@
     <title><?= $_GET['page'] ?></title>
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 
-    <link href="http://stage-o2.prod.quest.corp/SQ/Static/Content/kendo/2015.1.318/kendo.requestqueues.min.css"
-          rel="stylesheet" type="text/css"/>
+	<link href="http://stage-o2.prod.quest.corp/SQ/Static/Content/kendo/2015.1.318/kendo.requestqueues.min.css"
+	      rel="stylesheet" type="text/css"/>
     <link rel="stylesheet" href="//code.jquery.com/ui/1.10.3/themes/smoothness/jquery-ui.min.css">
     <link rel="stylesheet" href="http://stage-o2.prod.quest.corp/SQ/Static/CSS/web-queue-plugins-style.min.css"/>
-    <link rel="stylesheet"
-          href="http://stage-o2.prod.quest.corp/SQ/Static/Scripts/RequestQueues/qtip/jquery.qtip-custom.min.css"/>
+	<link rel="stylesheet"
+	      href="http://stage-o2.prod.quest.corp/SQ/Static/Scripts/RequestQueues/qtip/jquery.qtip-custom.min.css"/>
     <link rel="stylesheet" href="http://stage-o2.prod.quest.corp/SQ/Static/CSS/bootstrap.min.css" type="text/css"/>
     <link rel="stylesheet" href="/SQ/Static/CSS/custom-ui.min.css"/>
     <link rel="stylesheet" href="http://stage-o2.prod.quest.corp/SQ/Static/CSS/custom-ui-is.min.css"/>
@@ -70,13 +70,13 @@
     <script>
         kendo.culture("en-US");
     </script>
-    <script type="text/javascript"
-            src="http://stage-o2.prod.quest.corp/SQ/View-Scripts/_RequestQueuesLayoutV2.min.js"></script>
+	<script type="text/javascript"
+	        src="http://stage-o2.prod.quest.corp/SQ/View-Scripts/_RequestQueuesLayoutV2.min.js"></script>
 </head>
 
 <body>
 
-<div class="wrapper">
+<div class="container">
 	<?php
 		echo file_get_contents('../widgets/top-area.tmpl.htm');
 	?>
@@ -86,135 +86,14 @@
 	echo $widget->content();
 ?>
 
-
-            <script src="http://stage-o2.prod.quest.corp/SQ/static/scripts/RequestQueues/custom-ui.js"></script>
-<?php
-  $localURL = $_SERVER['REQUEST_URI'];
-  if($localURL == 'http://stage-o2.prod.quest.corp/SQ/singlequeue/pages/view_requests/view-requests.html'){
-	  echo "<script src=\"http://stage-o2.prod.quest.corp/SQ/static/scripts/RequestQueues/ribbon.js\"></script>";
-  }
-
-?>
-			<script src="http://stage-o2.prod.quest.corp/SQ/static/scripts/RequestQueues/single-queue.js"></script>
-			<script src="http://stage-o2.prod.quest.corp/SQ/static/scripts/RequestQueues/sq-listings-module.js"></script>
-			<script src="http://stage-o2.prod.quest.corp/SQ/static/scripts/RequestQueues/o2-global-modules.js"></script>
-
-
-
+<script src="/SQ/static/scripts/RequestQueues/custom-ui.js"></script>
+<script src="/SQ/static/scripts/RequestQueues/single-queue.js"></script>
+<script src="/SQ/static/scripts/RequestQueues/sq-listings-module.js"></script>
+<script src="/SQ/static/scripts/RequestQueues/o2-global-modules.js"></script>
 
 <?php
 	echo $widget->js();
 ?>
 
-<script>
-	$(document).ready(function() {
-		if ($('#tabs').length > 0) {
-			$('#tabs').tabs({
-				active: 0,
-				beforeActivate: function(event, ui) {
-					return CheckParentDataChanges();
-				},
-				activate: function(event, ui) {
-					//Add/Remove vertical pipe on the tabs when an individual tab is activated.
-					var indx = $('#tabs').find('> ul > li').index(ui.newTab);
-					//alert(indx);
-					var id = ui.newTab.find('a').attr('href');
-
-					if (!$(id).data('processed')) {
-						if (id == '#oasis2') {
-							window.parent.location.href = "http://stage-o2.prod.quest.corp/";
-							$('#sub-logo').attr('src', '/images/portal/images/sublogo-oasis2.png');
-						}
-						else {
-							window.parent.location.href = "http://stage-o2.prod.quest.corp/SQ/RequestQueues/Requests/";
-							$('#sub-logo').attr('src', '/images/portal/images/sublogo-marketing-request-queue.png');
-						}
-
-						$(id).data('processed', true);
-					}
-
-					var imgsrc = '';
-					var linkurl = '';
-
-					if (id == '#oasis2') {
-						imgsrc = '/images/portal/images/sublogo-oasis2.png';
-						linkurl = 'http://stage-o2.prod.quest.corp/'
-					}
-					else {
-						imgsrc = '/images/portal/images/sublogo-marketing-request-queue.png';
-						linkurl = 'http://stage-o2.prod.quest.corp/SQ/RequestQueues/Requests/'
-					}
-
-					$('#sub-logo').attr('src', imgsrc);
-					$('#sub-logo-link').attr('href', linkurl);
-
-					$('#tabs').find('> ul > li').removeClass('hide-pipe');
-
-					if (indx) {
-						for (var c = indx - 1; c >= 0; c--) {
-							var tab = $('#tabs').find('> ul > li:eq(' + c + ')');
-
-							if (tab.is(':visible')) {
-								tab.addClass('hide-pipe');
-								break;
-							}
-						}
-					}
-
-				}
-			}).show();
-		}
-
-		$("a#ui-id-1").click(function() {
-			if ("0" == "0") {
-				if (CheckParentDataChanges()) {
-					window.parent.location.href = "http://stage-o2.prod.quest.corp/SQ/RequestQueues/Requests/";
-				}
-			}
-		});
-	});
-
-	function doSearch() {
-		var q = document.getElementById("search-query").value;
-		var t = 0;
-		var targets = document.getElementsByName("searchtype");
-		for (var i = 0; i < targets.length; i++) {
-			if (targets[i].checked) {
-				t = targets[i].value;
-			}
-		}
-
-		if (q.length > 0) {
-			var searchUrl = "/include/search.aspx?q=" + encodeURIComponent(q) + "&t=" + t;
-			window.open(searchUrl, "_blank");
-		}
-
-		return false;
-	}
-
-	function onSearchKeyPress(e) {
-		if (e.keyCode == 13) {
-			doSearch();
-		}
-	}
-
-	function CheckParentDataChanges() {
-		try {
-			if (typeof parent.CheckDataChanges == 'undefined') {
-				return true;
-			}
-			else {
-				if (parent.CheckDataChanges()) {
-					parent.ClearDataChangesCheck();
-					return true;
-				}
-				else {
-					return false;
-				}
-			}
-		} catch (e) {
-		}
-	}
-</script>
 </body>
 </html>
